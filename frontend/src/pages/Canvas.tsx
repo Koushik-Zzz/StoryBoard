1eca895def90ad9186aaadceda4e88ab08712d0e
sandip chakraborty <sanchakraborty.sc@email.com>
2026-02-28 20:42:12 +0530

    Global Context frame

--- frontend/src/pages/Canvas.tsx
+++ frontend/src/pages/Canvas.tsx
@@ -3,25 +3,27 @@ import "tldraw/tldraw.css";
 import { useCanvas } from "../hooks/useCanvas";
 import { CanvasToolbar } from "../components/canvas/CanvasToolbar";
 import { FrameShapeUtil } from "../shapes/FrameShape";
+import { GlobalContextFrameShapeUtil } from "../shapes/GlobalContextFrameShape";
 import { ArrowActionMenu } from "../components/canvas/ArrowActionMenu";
+const customShapeUtils = [FrameShapeUtil, GlobalContextFrameShapeUtil];
 
-const customShapeUtils = [FrameShapeUtil];
 
 export default function Canvas() {
-  const { handleMount, handleImport, handleClear } = useCanvas();
+  const { handleMount, handleImport, handleClear, createGlobalContextFrame } = useCanvas();
 
   return (
     <div style={{ position: "fixed", inset: 0 }}>
       <Tldraw 
         onMount={handleMount} 
         shapeUtils={customShapeUtils}
-        persistenceKey="storyboard-canvas-v1"
+        persistenceKey="hack-western-canvas-v1"
       >
         <ArrowActionMenu />
       </Tldraw>
       <CanvasToolbar
         onClear={handleClear}
         onImport={handleImport}
+        onCreateGlobalContextFrame={createGlobalContextFrame}
       />
     </div>
   );
