6c9a619c5dbcedd58b94de8ee826da4463b0b818
sandip chakraborty <sanchakraborty.sc@email.com>
2026-02-28 21:14:50 +0530

    build: update build configs

--- frontend/src/pages/Canvas.tsx
+++ frontend/src/pages/Canvas.tsx
@@ -3,13 +3,12 @@ import "tldraw/tldraw.css";
 import { useCanvas } from "../hooks/useCanvas";
 import { CanvasToolbar } from "../components/canvas/CanvasToolbar";
 import { FrameShapeUtil } from "../shapes/FrameShape";
-import { GlobalContextFrameShapeUtil } from "../shapes/GlobalContextFrameShape";
 import { ArrowActionMenu } from "../components/canvas/ArrowActionMenu";
-const customShapeUtils = [FrameShapeUtil, GlobalContextFrameShapeUtil];
 
+const customShapeUtils = [FrameShapeUtil];
 
 export default function Canvas() {
-  const { handleMount, handleImport, handleClear, createGlobalContextFrame } = useCanvas();
+  const { handleMount, handleImport, handleClear } = useCanvas();
 
   return (
     <div style={{ position: "fixed", inset: 0 }}>
@@ -23,7 +22,6 @@ export default function Canvas() {
       <CanvasToolbar
         onClear={handleClear}
         onImport={handleImport}
-        onCreateGlobalContextFrame={createGlobalContextFrame}
       />
     </div>
   );
